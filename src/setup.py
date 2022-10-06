6793c23367eee4ce926f40190f8eb5ce46de454f
polvilavella <polvilavella@gmail.com>
2022-10-06 13:22:06 +0200

    package definition

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='src',
+    version='0.1.0',
+    author='Sentimentales',
+    packages=['data', 'features', 'models'],
+    license='LICENSE.txt',
+    description='An awesome package that does something',
+)
