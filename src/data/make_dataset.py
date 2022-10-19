196b9c6328d9bb1c15a14fe45e1c9bc402df38fb
polvilavella <polvilavella@gmail.com>
2022-10-19 18:40:03 +0200

    clean data: keep only english texts

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,23 +1,53 @@
 # -*- coding: utf-8 -*-
+
+""" Generate clean dataset from raw data """
+
+import os
 import logging
-import pandas as pd
-import emoji
 import re
 import string
+import pandas as pd
+import emoji
+from langdetect import detect
+
+
+logger = logging.getLogger(__name__)
 
 
 def clean_data(df_raw, text_col='Comment'):
-    """Given a dataframe and a text column, """
+    """ Given a dataframe and a text column, treats the text column of the dataframe and keeps
+        only English text.
+    """
     df_clean = df_raw.dropna().reset_index(drop=True)
-
+    ids_en = []
+    # Clean the text
     for idx, row in df_clean.iterrows():
         text = row[text_col]
-        text_clean = emoji.demojize(text, delimiters=(" ", " "))
-        text_clean = text_clean.translate(str.maketrans('', '', string.punctuation))
+        
+        # Remove special characters
+        text_clean = text.translate(str.maketrans('', '', '#$%&()*+<=>?@[\\]^_`{|}~'))
+        text_clean = text_clean.replace("\"", " ")
+        # Remove endlines and tabs
         text_clean = text_clean.replace("\n", " ")
         text_clean = text_clean.replace("\r", " ")
+        text_clean = text_clean.replace("\xa0", " ")
+        # Reduce multiple spces to one
         text_clean = re.sub(' +', ' ', text_clean)
+
+        try:
+            language = detect(text_clean)
+        except:
+            language = 'error'
+        # If the text is in English, then keep the row
+        if language == 'en':
+            ids_en.append(idx)
+        
+        # Translate emojis to text
+        text_clean = emoji.demojize(text_clean, delimiters=(" ", " "))
+        
         df_clean.loc[idx,text_col] = text_clean
+
+    df_clean = df_clean[df_clean.index.isin(ids_en)].reset_index(drop=True)
 
     return df_clean
 
@@ -26,23 +56,25 @@ def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
-    logger = logging.getLogger(__name__)
-    
     logger.info('making final data set from raw data...')
-    
-    data_raw = "../../data/raw/comments.csv"
+
+    # Path of the script
+    dirname = os.path.dirname(__file__)
+    # Path to the raw data
+    data_raw = os.path.join(dirname, "../../data/raw/comments.csv")
+
     df_raw = pd.read_csv(data_raw, index_col=0, sep=',')
-    
     df_clean = clean_data(df_raw, text_col='Comment')
 
     logger.info('final data set created')
 
-    data_processed = "../../data/processed/comments_clean.csv"
+    # Path to the processed data
+    data_processed = os.path.join(dirname, "../../data/processed/comments_clean.csv")
     df_clean.to_csv(data_processed)
-    
+
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    LOG_FMT = '%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=LOG_FMT)
 
     main()
