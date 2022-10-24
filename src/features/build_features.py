8d6eb40ad65b349706a22f0976c9d1a726fea2f2
polvilavella <polvilavella@gmail.com>
2022-10-24 23:20:37 +0200

    update build_features script: cleaning and documentation

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,3 +1,6 @@
+
+""" Module to prepare the input of the model for the prediction from the cleaned data. """
+
 import pandas as pd
 import torch
 
@@ -19,8 +22,9 @@ def prepare_target(sentiments):
     return torch.tensor(target)
 
 
-def preprocess(data_clean='../../data/processed/comments_clean.csv', text_col='Comment', target_col='Sentiment'):
-    """TODO: finish documentation"""
+def preprocess(data_clean='../../data/processed/comments_clean.csv',
+               text_col='Comment', target_col='Sentiment'):
+    """ Preprocess clean data for the prdiction. """
     df_clean = pd.read_csv(data_clean, index_col=0, sep=',')
     comments = df_clean[text_col].tolist()  # The tokenizer recieves a list as input
     target = prepare_target(df_clean[target_col])
